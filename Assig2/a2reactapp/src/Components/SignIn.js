30bfe1d734da63b3bcb2414b608015dd4e7b38d5
AndreAlexandrov <andre3691@hotmail.com>
2024-10-22 16:53:33 +1030

    data set up for query

--- Assig2/a2reactapp/src/Components/SignIn.js
+++ Assig2/a2reactapp/src/Components/SignIn.js
@@ -6,6 +6,8 @@ const SignIn = ({ }) => {
 
     const [selectedSelect, setSelected] = useState(['']);
 
+    const [query, setQuery] = useState([]);
+
     React.useEffect(() => {
         fetch(`http://localhost:5147/api/Login`)
             .then(response => response.json())
@@ -17,19 +19,25 @@ const SignIn = ({ }) => {
     //https://bobbyhadz.com/blog/react-select-onchange
     
     function logInQuery(evt) {
+        const userName = document.querySelector('[name="userName"]').value;
+        const password = document.querySelector('[name="password"]').value;
+        password = SHA256(password)
 
+        setQuery([userName, password])
     }
     return (
         <div className="row justify-content-start mb-3">
-        <div class="form-outline mb-4">
-            <input type="text" name="userName" id="userName" class="form-control" />
-            <label class="form-label" for="userName">User Name</label>
-        </div>
+            <div class="form-outline mb-4">
+                <input type="text" name="userName" id="userName" class="form-control" />
+                <label class="form-label" for="userName">User Name</label>
+            </div>
 
             <div class="form-outline mb-4">
                 <input type="password" id="password" name="password" class="form-control" />
                 <label class="form-label" for="password">Password</label>
             </div>
+
+            <button type="button" class="btn btn-primary btn-block mb-4" onClick={logInQuery}>Sign in</button>
 
         </div>
 
