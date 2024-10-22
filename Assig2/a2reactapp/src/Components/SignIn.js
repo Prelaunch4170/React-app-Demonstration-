501c70e14665a5d3026f64e54c6fd1a72704ec7b
AndreAlexandrov <andre3691@hotmail.com>
2024-10-22 16:36:40 +1030

    setting up the sign in logic

---
+++ Assig2/a2reactapp/src/Components/SignIn.js
@@ -0,0 +1,27 @@
+﻿import React, { useState } from 'react'
+
+
+const SignIn = ({ }) => {
+    const [subData, setState] = useState([]);
+
+    const [selectedSelect, setSelected] = useState(['']);
+
+    React.useEffect(() => {
+        fetch(`http://localhost:5147/api/Login`)
+            .then(response => response.json())
+            .then(data => setState(data))
+            .catch(err => {
+                console.log(err);
+            });
+    }, [])
+    //https://bobbyhadz.com/blog/react-select-onchange
+    
+
+    return (
+        
+    )
+
+
+}
+
+export default SignIn
