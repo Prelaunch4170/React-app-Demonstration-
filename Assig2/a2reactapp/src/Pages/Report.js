250ed9127c0fd16627917f6baf1ddf11764fb14d
AndreAlexandrov <andre3691@hotmail.com>
2024-11-06 01:17:27 +1030

    made test graph from practicals
    also added params

--- Assig2/a2reactapp/src/Pages/Report.js
+++ Assig2/a2reactapp/src/Pages/Report.js
@@ -11,6 +11,8 @@ function App() {
     const isSignedIn = Cookies.get("isSignedIn");
     const navigate = useNavigate();
 
+
+
     const offenceSearch = "km/h"
 
     useEffect(() => {
@@ -38,6 +40,8 @@ function App() {
         const Data118 = await fetch(`http://localhost:5147/api/Get_ExpiationsForLocationId?locationId=${118}&cameraTypeCode=${'I/section'}&endTime=2147483647${offencesList}`)
         const Json118 = await Data118.json();
 
+
+        //
         let expiationsByMonth118 = [];
 
         Json118.forEach(ex => {
@@ -64,11 +68,112 @@ function App() {
             }
 
         })
+        //https://dev.to/nasreenkhalid/how-to-sort-an-array-of-month-names-javascript-4c3n
+        // making sure its in the right order
+        expiationsByMonth118.sort((a, b) => {
+            const monthOrder = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            return monthOrder.indexOf(a.monthName) - monthOrder.indexOf(b.monthName)
+        })
 
+        buildFirstGraph(expiationsByMonth118)
+        
+        console.log(expiationsByMonth118);
+        //#endregion
 
     }
 
 
+    function buildFirstGraph(dataSet) {
+        //#region svg setup
+
+        const svg = d3.select('svg');
+
+        let w = svg.node().getBoundingClientRect().width;
+        let h = svg.node().getBoundingClientRect().height;
+
+        const chartMargins = {
+            left: 40,
+            right: 25,
+            top: 25,
+            bottom: 80
+
+        }
+        h -= (chartMargins.top + chartMargins.bottom);
+        w -= (chartMargins.left + chartMargins.right);
+        //#endregion
+
+        //#region SVG stuff
+        d3.selectAll("svg > *").remove();
+
+        console.log(dataSet);
+        let monthArray = Array.from(dataSet, (d, i) => d.monthName);
+
+        console.log("array: " + monthArray);
+
+        let totalItemsRange = d3.extent(dataSet, (d, i) => d.expiations);
+        let maxTotalItems = totalItemsRange[1];
+
+        const barMargin = 10;
+        const barWidth = w / dataSet.length;
+
+        let yScale = d3.scaleLinear()
+            .domain([0, maxTotalItems])
+            .range([h, 0]);
+
+        let xScale = d3.scaleBand()
+            .domain(monthArray)
+            .range([0, w])
+            .paddingInner(0.1);
+
+        const chartGroup = svg.append('g')
+            .classed('chartGroup', true)
+            .attr('transform', `translate(${chartMargins.left},${chartMargins.top})`);
+
+        let barGroups = chartGroup
+            .selectAll('g')
+            .data(dataSet);
+
+        let newBarGroups = barGroups.enter()
+            .append('g')
+            .attr('transform', (d, i) => {
+                return `translate(${xScale(d.monthName)}, ${yScale(d.expiations)})`;
+            });
+
+        newBarGroups
+            .append('rect')
+            .attr('x', 0)
+            .attr('height', 0)
+            .attr('y', (d, i) => { return h - yScale(d.expiations); })
+            .attr('width', xScale.bandwidth())
+            .style('fill', 'transparent')
+            .transition().duration((d, i) => i * 300)
+            .delay((d, i) => i + 200)
+            .attr('y', 0)
+            .attr('height', (d, i) => { return h - yScale(d.expiations); })
+            .style('fill', (d, i) => { return `rgb(20,20,${i * 15 + 80})` });
+        newBarGroups
+            .append('text')
+            .attr("text-anchor", "middle")
+            .attr('x', (d, i) => { return xScale.bandwidth() / 2; })
+            .attr('y', 20)
+            .attr('fill', 'white')
+            .style('font-size', '1em')
+            .text((d, i) => d.expiations.toLocaleString());
+
+        //11 create y Axis and save to svg
+        let yAxis = d3.axisLeft(yScale);
+        chartGroup.append('g')
+            .classed('axis y', true)
+            .call(yAxis);
+
+        let xAxis = d3.axisBottom(xScale);
+        chartGroup.append('g')
+            .attr('transform', `translate(10, ${h})`)
+            .classed('axis x', true)
+            .call(xAxis);
+            //#endregion
+
+    }
 
     return (
         <div className="container-fluid ">
